e1c6c6040d2031bad1940d4aa187f9791da9af7c
aliang <t112598014@ntut.org.tw>
2024-10-09 13:37:13 +0800

    refactor unit test code

--- tests/normal_schemer_test.go
+++ tests/normal_schemer_test.go
@@ -1,10 +1,65 @@
 package tests
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/BrobridgeOrg/schemer"
 	"github.com/stretchr/testify/assert"
+)
+
+type Input struct {
+	id         string
+	string_col string
+	binary_col string
+	int_col    string
+	uint_col   string
+	float_col  string
+	bool_col   string
+	any_col    string
+}
+
+type Expected struct {
+	id         uint64
+	string_col string
+	binary_col []byte
+	int_col    int64
+	uint_col   uint64
+	float_col  float64
+	bool_col   bool
+	any_col    interface{}
+}
+
+type ExtensionInput struct {
+	id         string
+	string_col string
+	binary_col string
+	int_col    string
+	uint_col   string
+	float_col  string
+	bool_col   string
+}
+
+type ExtensionExpected struct {
+	id         uint64
+	string_col string
+	binary_col []byte
+	int_col    int64
+	uint_col   uint64
+	float_col  float64
+	bool_col   bool
+}
+
+var (
+	// SPECIAL_CHAR = `"!@#$%^&*()_+{}:<>?~-=[]\;',./"`
+	// SPECIAL_CHAR_EXPECTED_OUTPUT = `!@#$%^&*()_+{}:<>?~-=[]\;',./`
+	SPECIAL_CHAR                 = `"!@#$%^&*()_+{}:<>?~-=[]',./"`
+	SPECIAL_CHAR_EXPECTED_OUTPUT = `!@#$%^&*()_+{}:<>?~-=[]',./`
+	LARGE_STRING_EXPECTED_OUTPUT string
+	LARGE_BYTE_EXPECTED_OUTPUT   []byte
+	LARGE_STRING                 string
+	LARGE_BYTE                   string
 )
 
 var normalSchema = `{
@@ -58,112 +113,158 @@ var normalSchemaForExtension1 = `{
 	}
 }`
 
-func TestNormalSchemer(t *testing.T) {
+func init() {
+	LARGE_STRING_EXPECTED_OUTPUT = ""
+	LARGE_BYTE_EXPECTED_OUTPUT := []byte{}
+	LARGE_BYTE = ""
+	for i := 0; i < 32768; i++ {
+		LARGE_STRING_EXPECTED_OUTPUT += "a"
+		LARGE_BYTE_EXPECTED_OUTPUT = append(LARGE_BYTE_EXPECTED_OUTPUT, 0x30)
+		LARGE_BYTE += "0"
+	}
+	LARGE_STRING = fmt.Sprintf(`"%s"`, LARGE_STRING)
+	LARGE_BYTE = fmt.Sprintf(`"%s"`, LARGE_BYTE)
+}
 
-	testNormalSchema := schemer.NewSchema()
-	err := schemer.UnmarshalJSON([]byte(normalSchema), testNormalSchema)
+func transformTest(t *testing.T, testSourceSchema *schemer.Schema, transformer *schemer.Transformer, input Input, expected Expected) {
+	source, err := normalize_normal_schema(testSourceSchema, input)
 	if err != nil {
 		t.Error(err)
+		return
+	}
+	result, err := transformAndAssert(t, transformer, source)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	assertResult(t, result, expected)
+}
+
+func transformExtensionTest(t *testing.T, testSourceSchema *schemer.Schema, transformer *schemer.Transformer, input ExtensionInput, expected ExtensionExpected) {
+	source, err := normalize_Extension(testSourceSchema, input)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	result, err := transformAndAssert(t, transformer, source)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	assertExtensionResult(t, result, expected)
+}
+
+func normalize_Extension(s *schemer.Schema, input ExtensionInput) (map[string]interface{}, error) {
+	jsonInput := fmt.Sprintf(`
+	{
+		"id":         %s,
+		"string_col": %s,
+		"binary_col": %s,
+		"int_col":    %s,
+		"uint_col":   %s,
+		"float_col":  %s,
+		"bool_col":   %s
+	}`, input.id, input.string_col, input.binary_col, input.int_col, input.uint_col, input.float_col, input.bool_col)
+	var rawData map[string]interface{}
+	err := json.Unmarshal([]byte(jsonInput), &rawData)
+	if err != nil {
+		return nil, err
+	}
+	return s.Normalize(rawData), nil
+}
+
+func normalize_normal_schema(s *schemer.Schema, input Input) (map[string]interface{}, error) {
+	jsonInput := fmt.Sprintf(`
+	{
+		"id":         %s,
+		"string_col": %s,
+		"binary_col": %s,
+		"int_col":    %s,
+		"uint_col":   %s,
+		"float_col":  %s,
+		"bool_col":   %s,
+		"any_col":    %s
+	}`, input.id, input.string_col, input.binary_col, input.int_col, input.uint_col, input.float_col, input.bool_col, input.any_col)
+	var rawData map[string]interface{}
+	err := json.Unmarshal([]byte(jsonInput), &rawData)
+	if err != nil {
+		return nil, err
+	}
+	return s.Normalize(rawData), nil
+}
+
+func transformAndAssert(t *testing.T, transformer *schemer.Transformer, source map[string]interface{}) (map[string]interface{}, error) {
+	returnedValue, err := transformer.Transform(nil, source)
+	if !assert.Nil(t, err) {
+		return nil, fmt.Errorf("transform failed: %v", err)
 	}
 
-	// Using the same schema for destination
+	if !assert.Len(t, returnedValue, 1) {
+		return nil, fmt.Errorf("return length not match")
+	}
+	result := returnedValue[0]
+	return result, nil
+}
+
+func assertResult(t *testing.T, result map[string]interface{}, expected Expected) {
+	assert.Equal(t, expected.id, result["id"])
+	assert.Equal(t, expected.string_col, result["string_col"])
+	assert.Equal(t, expected.binary_col, result["binary_col"])
+	assert.Equal(t, expected.int_col, result["int_col"])
+	assert.Equal(t, expected.uint_col, result["uint_col"])
+	assert.Equal(t, expected.float_col, result["float_col"])
+	assert.Equal(t, expected.bool_col, result["bool_col"])
+	assert.Equal(t, expected.any_col, result["any_col"])
+}
+
+func assertExtensionResult(t *testing.T, result map[string]interface{}, expected ExtensionExpected) {
+	assert.Equal(t, expected.id, result["id"])
+	assert.Equal(t, expected.string_col, result["string_col"])
+	assert.Equal(t, expected.binary_col, result["binary_col"])
+	assert.Equal(t, expected.int_col, result["int_col"])
+	assert.Equal(t, expected.uint_col, result["uint_col"])
+	assert.Equal(t, expected.float_col, result["float_col"])
+	assert.Equal(t, expected.bool_col, result["bool_col"])
+}
+
+func TestNormalSchemer(t *testing.T) {
+	testSourceSchema := schemer.NewSchema()
+	err := schemer.UnmarshalJSON([]byte(normalSchema), testSourceSchema)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 	testDestSchema := schemer.NewSchema()
 	err = schemer.UnmarshalJSON([]byte(normalSchema), testDestSchema)
 	if err != nil {
 		t.Error(err)
+		return
 	}
-
-	// Create transformer
-	transformer := schemer.NewTransformer(testNormalSchema, testDestSchema)
-
-	// Set transform script
+	transformer := schemer.NewTransformer(testSourceSchema, testDestSchema)
 	transformer.SetScript(`return source`)
 
-	// Preparing source data
-	source := testNormalSchema.Normalize(map[string]interface{}{
-		"id":         3,
-		"string_col": "abc",
-		"binary_col": "[0]x32768",
-		"int_col":    -1,
-		"uint_col":   5,
-		"float_col":  -1.23,
-		"bool_col":   false,
-		"any_col":    "abc",
-	})
-
-	// Transforming
-	// returnedValue, err := transformer.Transform(nil, source)
-	// if assert.Nil(t, err) {
-	// 	return
-	// }
-
-	// if !assert.Len(t, returnedValue, 1) {
-	// 	return
-	// }
-
-	// result := returnedValue[0]
-
-	// Normal fields
-	assert.Equal(t, uint64(3), source["id"])
-	assert.Equal(t, "abc", source["string_col"].(string))
-	assert.Equal(t, []byte{0x5b, 0x30, 0x5d, 0x78, 0x33, 0x32, 0x37, 0x36, 0x38}, source["binary_col"])
-	assert.Equal(t, int64(-1), source["int_col"])
-	assert.Equal(t, uint64(5), source["uint_col"])
-	assert.Equal(t, float64(-1.23), source["float_col"])
-	assert.Equal(t, false, source["bool_col"])
-	assert.Equal(t, "abc", source["any_col"].(string))
+	MainSuccessInput1 := Input{`1`, `""`, `""`, `5`, `5`, `5`, `0`, `""`}
+	MainSuccessExpected1 := Expected{1, "", []byte{}, 5, 5, 5, false, ""}
+	transformTest(t, testSourceSchema, transformer, MainSuccessInput1, MainSuccessExpected1)
 }
 
 func TestNotMatchWithExpectResult(t *testing.T) {
-	
-	testNormalSchema := schemer.NewSchema()
-	err := schemer.UnmarshalJSON([]byte(normalSchemaForExtension1), testNormalSchema)
+	testSourceSchema := schemer.NewSchema()
+	err := schemer.UnmarshalJSON([]byte(normalSchemaForExtension1), testSourceSchema)
 	if err != nil {
 		t.Error(err)
+		return
 	}
-
-	// Using the same schema for destination
 	testDestSchema := schemer.NewSchema()
 	err = schemer.UnmarshalJSON([]byte(normalSchemaForExtension1), testDestSchema)
 	if err != nil {
 		t.Error(err)
+		return
 	}
-
-	// Create transformer
-	transformer := schemer.NewTransformer(testNormalSchema, testDestSchema)
-
-	// Set transform script
+	transformer := schemer.NewTransformer(testSourceSchema, testDestSchema)
 	transformer.SetScript(`return source`)
 
-	// Preparing source data
-	source := testNormalSchema.Normalize(map[string]interface{}{
-		"id":         1,
-		"string_col": `!@#$%^&*()_+{}:<>?~-=[]\;',./`,
-		"binary_col": "abc",
-		"int_col":    "",
-		"uint_col":   "",
-		"float_col":  "",
-		"bool_col":   "",
-	})
-
-	// Transforming
-	// returnedValue, err := transformer.Transform(nil, source)
-	// if assert.Nil(t, err) {
-	// 	return
-	// }
-
-	// if !assert.Len(t, returnedValue, 1) {
-	// 	return
-	// }
-
-	// result := returnedValue[0]
-
-	// Normal fields
-	assert.Equal(t, uint64(1), source["id"])
-	assert.Equal(t, `!@#$%^&*()_+{}:<>?~-=[]\;',./`, source["string_col"].(string))
-	assert.Equal(t, []byte{0x61,0x62,0x63}, source["binary_col"])
-	assert.Equal(t, int64(0), source["int_col"])
-	assert.Equal(t, uint64(0x0), source["uint_col"])
-	assert.Equal(t, float64(0), source["float_col"])
-	assert.Equal(t, false, source["bool_col"])
+	ExtensionOneInput1 := ExtensionInput{`1`, `5`, `"abc"`, `""`, `""`, `""`, `""`}
+	ExtensionOneExpected1 := ExtensionExpected{uint64(1), "5", []byte{0x61, 0x62, 0x63}, int64(0), uint64(0x0), float64(0), false}
+	transformExtensionTest(t, testSourceSchema, transformer, ExtensionOneInput1, ExtensionOneExpected1)
 }
